bfb3c45d1170ccfd54e3edd4680703dd208c988a
Darby Sauter <darbysauter@gmail.com>
2022-07-20 22:50:48 -0700

    Added thread to run on app start (viewcontroller init) and thread to run when button pressed (disables button while thread running)

--- src/run_payload/mod.rs
+++ src/run_payload/mod.rs
@@ -1,4 +1,14 @@
+use std::thread;
+use std::time::Duration;
 
-pub fn run_payload() {
-    nslogln!("HELLO FROM RUST!");
+pub fn run_on_app_start() {
+    nslogln!("App started!");
+    thread::sleep(Duration::from_millis(1000));
+    nslogln!("App done init!");
+}
+
+pub fn run_on_button_press() {
+    nslogln!("Running payload!");
+    thread::sleep(Duration::from_millis(1000));
+    nslogln!("Done running payload!");
 }
--- src/viewcontroller/mod.rs
+++ src/viewcontroller/mod.rs
@@ -1,6 +1,7 @@
 use crate::objc::*;
-use crate::run_payload::run_payload;
+use crate::run_payload::{ run_on_app_start, run_on_button_press };
 use cstr::cstr;
+use std::thread;
 
 extern "C" fn my_viewcontroller_init(obj: *mut Id, _cmd: Sel) -> Id {
     unsafe {
@@ -22,6 +23,8 @@ const UICONTROL_STATE_NORMAL: u64 = 0;
 const UICONTROL_STATE_HIGHLIGHTED: u64 = 1 << 0;
 const UICONTROL_EVENT_TOUCH_UP_INSIDE: u64 = 1 << 6;
 
+static mut BUTTON_REF: usize = 0;
+
 extern "C" fn my_viewcontroller_load_view(obj: *mut Id, _cmd: Sel) {
     unsafe {
         let sup = class_getSuperclass(object_getClass(obj));
@@ -39,6 +42,7 @@ extern "C" fn my_viewcontroller_load_view(obj: *mut Id, _cmd: Sel) {
 
         // button = [UIButton buttonWithType:UIButtonTypeSystem];
         let button: *mut Id = rust_msg_send_1(objc_getClass(cstr!("UIButton").as_ptr()), sel_getUid(cstr!("buttonWithType:").as_ptr()), UIBUTTON_TYPE_SYSTEM);
+        BUTTON_REF = button as usize;
         // button.translatesAutoresizingMaskIntoConstraints = NO;
         let nsstring: *mut Id = rust_msg_send_1(rust_msg_send(objc_getClass(cstr!("NSString").as_ptr()), sel_getUid(cstr!("alloc").as_ptr())), sel_getUid(cstr!("initWithUTF8String:").as_ptr()), cstr!("translatesAutoresizingMaskIntoConstraints").as_ptr());
         let nsnumber_bool: *mut Id = rust_msg_send_1(objc_getClass(cstr!("NSNumber").as_ptr()), sel_getUid(cstr!("numberWithBool:").as_ptr()), NO);
@@ -90,10 +94,43 @@ extern "C" fn my_viewcontroller_load_view(obj: *mut Id, _cmd: Sel) {
         // [self.view addConstraint:constraint2];
         rust_msg_send_1::<(), *mut Id>(self_view, sel_getUid(cstr!("addConstraint:").as_ptr()), constraint2);
     }
+
+    thread::spawn(|| {
+        run_on_app_start();
+    });
 }
 
-extern "C" fn my_viewcontroller_button_tapped(_obj: *mut Id, _cmd: Sel) {
-    run_payload();
+
+fn my_viewcontroller_disable_button() {
+    unsafe {
+        let button = BUTTON_REF as *mut Id;
+        rust_msg_send_1::<(), Bool>(button, sel_getUid(cstr!("setEnabled:").as_ptr()), NO);
+
+        let color3: *mut Id = rust_msg_send_4(objc_getClass(cstr!("UIColor").as_ptr()), sel_getUid(cstr!("colorWithRed:green:blue:alpha:").as_ptr()), 0.0f64, 0.0f64, 0.0f64, 0.1f64);
+        rust_msg_send_1::<(), *mut Id>(button, sel_getUid(cstr!("setBackgroundColor:").as_ptr()), color3);
+    }
+}
+
+extern "C" fn my_viewcontroller_enable_button(_obj: *mut Id, _cmd: Sel) {
+    unsafe {
+        let button = BUTTON_REF as *mut Id;
+        rust_msg_send_1::<(), Bool>(button, sel_getUid(cstr!("setEnabled:").as_ptr()), YES);
+
+        let color3: *mut Id = rust_msg_send_4(objc_getClass(cstr!("UIColor").as_ptr()), sel_getUid(cstr!("colorWithRed:green:blue:alpha:").as_ptr()), 0.0f64, 0.0f64, 1.0f64, 1.0f64);
+        rust_msg_send_1::<(), *mut Id>(button, sel_getUid(cstr!("setBackgroundColor:").as_ptr()), color3);
+    }
+}
+
+extern "C" fn my_viewcontroller_button_tapped(obj: *mut Id, _cmd: Sel) {
+    my_viewcontroller_disable_button();
+    let obj = obj as usize;
+    thread::spawn(move || {
+        run_on_button_press();
+        unsafe {
+            let obj = obj as *mut Id;
+            rust_msg_send_3::<(), Sel, *mut Id, Bool>(obj, sel_getUid(cstr!("performSelectorOnMainThread:withObject:waitUntilDone:").as_ptr()), sel_getUid(cstr!("enableButton").as_ptr()), obj, NO);
+        }
+    });
 }
 
 pub fn init_my_viewcontroller() {
@@ -106,6 +143,8 @@ pub fn init_my_viewcontroller() {
         class_addMethod(my_viewcontroller_class, sel_getUid(cstr!("loadView").as_ptr()), my_vc_load_view, cstr!("v@:").as_ptr());
         let my_vc_button_tapped: usize = my_viewcontroller_button_tapped as *const () as usize;
         class_addMethod(my_viewcontroller_class, sel_getUid(cstr!("buttonTapped").as_ptr()), my_vc_button_tapped, cstr!("v@:").as_ptr());
+        let my_vc_enable_button: usize = my_viewcontroller_enable_button as *const () as usize;
+        class_addMethod(my_viewcontroller_class, sel_getUid(cstr!("enableButton").as_ptr()), my_vc_enable_button, cstr!("v@:").as_ptr());
 
         objc_registerClassPair(my_viewcontroller_class);
     }
